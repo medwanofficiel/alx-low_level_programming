c7706c6599e933c0a8363c9163bb20439fd84611
medwanofficiel <medwanofficiel@gmail.com>
2023-06-30 09:25:47 -0700

    o-reset_to_98.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rest_to_98 -a fenction that takes a pointer toan int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * return: n
+ */
+void reset_yo_98(int "n)
+{
+	"n =98;
+}
