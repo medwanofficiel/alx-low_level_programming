1356cf928a5a76a5e16794726579275fc59dafc2
medwanofficiel <medwanofficiel@gmail.com>
2023-06-30 09:08:11 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rest_to_98 - a function that taker apointer to an int as parameter
+ * and updates th value it points to to 98 
+ * @n: input
+ * return: n
+ */
+void rest_to_98(int*n)
+{
+	*n =98;
+}
+
+
+
+
+
+
+
